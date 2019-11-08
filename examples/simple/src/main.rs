d41acd64215ecfa00bff8b5eab36a9f28a704ee3
HinamoreCh4n <motherrussia711@gmail.com>
2019-11-08 22:04:04 +0000

    Added example comments

--- examples/simple/src/main.rs
+++ examples/simple/src/main.rs
@@ -5,28 +5,38 @@ use websocket_event::server::Server;
 use websocket_event::Event;
 use websocket_event::SerializableEvent;
 
+/// Basic event struct which serializes with serde to json.
 #[derive(Serialize, Debug)]
 struct SimplePushEvent {
     message: String,
 }
 
 impl SerializableEvent for SimplePushEvent {
+    /// Serialize method used as a intermediary to serialize the struct into a json string and
+    /// return it.
     fn serialize(&self) -> String {
         serde_json::to_string(&self).unwrap()
     }
 }
 
 fn main() {
+    // Server is started on localhost with port 3012
     let server = Server::new("127.0.0.1:3012");
     let tx = server.get_tx();
 
     loop {
+        // We create a new boxed instance of our SimplePushEvent struct with whatever message
+        // inside.
         let msg = Box::new(SimplePushEvent {
             message: String::from("Hello world"),
         });
 
+        // The previous message event is encapsulated in our Event struct to which we supply two
+        // arguments, the path/resource subscribers we would like to target ("/hello_world") and
+        // our message event struct instance which implements SerializableEvent.
         let event = Event::new("/hello_world", msg);
 
+        // The event is then sent over the tx channel provided by our server instance
         match tx.send(event) {
             Ok(_) => {}
             Err(x) => println!("Err {:?}", x),
